5986104e40ec37b8366c25438b4d6ad29f14486f
SeongHyeok Im <ishez9210@gmail.com>
2014-02-05 22:32:52 -0500

    Modify structure and add some comments

--- hw01/cards.c
+++ hw01/cards.c
@@ -10,9 +10,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int input(char *card_name)
+/*
+   Input card_name from user.
+   Input is truncated to the size of definition of card_name in caller.
+
+   Return 1 only if card_name is 'x'.
+
+   message: message to show
+   card_name: pointer of character array where input is stored
+ */
+int input(char *message, char *card_name)
 {
-    puts("Enter the card_name: ");
+    puts(message);
     scanf("%2s", card_name);
     if (card_name[0] == 'x') {
         return 1;
@@ -20,50 +29,75 @@ int input(char *card_name)
     return 0;
 }
 
-int get_value(char *card_name, int *ret)
+/*
+   Set val from given card_name.
+   This function converts card_name to the value and sets this to val.
+
+   Return 1 if given value is not available to convert.
+
+   card_name: card name to convert
+   val: buffer where converted value from card_name is written
+ */
+int set_value(char *card_name, int *val)
 {
-    int val = 0;
+    int v = 0;
     switch (card_name[0]) {
     case 'K':
     case 'Q':
     case 'J':
-        val = 10;
+        v = 10;
         break;
     case 'A':
-        val = 11;
+        v = 11;
         break;
     case 'X':
         return 1;
         break;
     default:
-        val = atoi(card_name);
-        if ((val < 1) || (val > 10)) {
+        v = atoi(card_name);
+        if ((v < 1) || (v > 10)) {
             puts("I don't understand that value!");
             return 1;
         }
         break;
     }
-    *ret = val;
+    *val = v;
     return 0;
 }
 
+/*
+   Set count based on given card value.
+
+   val: card value
+   count: pointer of integer variable, it is changed
+ */
+void change_count(int val, int *count)
+{
+    if ((val > 2) && (val < 7)) {
+        ++(*count);
+    }
+    else if (val == 10) {
+        --(*count);
+    }
+    printf("Current count: %i\n", *count);
+}
+
+/*
+   Main process function.
+ */
 void process()
 {
     char card_name[3];
     int count = 0, val = 0;
-    while (input(card_name) == 0) {
-        if (get_value(card_name, &val) != 0) {
+    
+    while (1) {
+        if (input("Enter the card_name: ", card_name) != 0) {
+            break;
+        }
+        if (set_value(card_name, &val) != 0) {
             continue;
         }
-        
-        if ((val > 2) && (val < 7)) {
-            count++;
-        }
-        else if (val == 10) {
-            count--;
-        }
-
-        printf("Current count: %i\n", count);
+        change_count(val, &count);
     } //while
 }
 
