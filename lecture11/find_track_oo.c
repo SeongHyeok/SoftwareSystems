eb5af3493c8dd78f906da02dc50237d0425df552
SeongHyeok Im <ishez9210@gmail.com>
2014-03-06 11:41:56 -0500

    Missing commit of exercise in lecture11.

--- lecture11/find_track_oo.c
+++ lecture11/find_track_oo.c
@@ -22,7 +22,7 @@ char tracks[][80] = {
 
 
 typedef struct {
-    regex_t inner_struct[1];
+    regex_t inner_struct[1];    // caution! It is an array not a variable.
 } Regex;
 
 
@@ -31,8 +31,21 @@ typedef struct {
 // flags: flags passed to regcomp
 // returns: new Regex 
 Regex *make_regex(char *pattern, int flags) {
-    // FILL THIS IN
-    return NULL;
+    Regex *regex;
+    int ret;
+    
+    regex = malloc(sizeof(Regex));
+    if (regex == NULL) {
+        fprintf(stderr, "Count not allocate memory\n");
+        exit(1);
+    }
+
+    ret = regcomp(regex->inner_struct, pattern, flags);
+    if (ret != 0) {
+        fprintf(stderr, "Could not compile regex\n");
+    }
+
+    return regex;
 }
 
 // Checks whether a regex matches a string.
@@ -40,14 +53,30 @@ Regex *make_regex(char *pattern, int flags) {
 // s: string
 // returns: 1 if there's a match, 0 otherwise
 int regex_match(Regex *regex, char *s) {
-    // FILL THIS IN
+    int ret;
+    char msgbuf[128];
+
+    ret = regexec(regex->inner_struct, s, 0, NULL, 0);
+    if (ret == 0) {
+        return 1;   // found
+    }
+    else if (ret == REG_NOMATCH) {
+        return 0;   // not found
+    }
+    else {
+       regerror(ret, regex->inner_struct, msgbuf, sizeof(msgbuf));
+       fprintf(stderr, "Regex match failed: %s\n", msgbuf);
+       exit(1);
+    }
+
     return 0;
 }
 
 // Frees a Regex.
 // regex: Regex
 void regex_free(Regex *regex) {
-    // FILL THIS IN
+    regfree(regex->inner_struct);
+    free(regex);
 }
 
 
@@ -61,9 +90,9 @@ void find_track_regex(char pattern[])
     Regex *regex = make_regex(pattern, REG_EXTENDED | REG_NOSUB);
 
     for (i=0; i<NUM_TRACKS; i++) {
-	if (regex_match(regex, tracks[i])) {
-	    printf("Track %i: '%s'\n", i, tracks[i]);
-	}
+    	if (regex_match(regex, tracks[i])) {
+    	    printf("Track %i: '%s'\n", i, tracks[i]);
+    	}
     }
 
     regex_free(regex);
