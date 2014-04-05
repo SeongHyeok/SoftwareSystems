750bb6f226f0607d288a8224b5922ebd04c0ef6b
SeongHyeok Im <ishez9210@gmail.com>
2014-04-05 02:29:31 -0400

    Implement punctuation filtering and fix bugs and memory leaks.

--- hw05/glib/wordfreq.c
+++ hw05/glib/wordfreq.c
@@ -21,10 +21,44 @@ GError *err = NULL;
 /* Utility Functions */
 /*****************************************************************************/
 
-gchar *stripPunctuation(gchar *str)
+GString *stripPunctuationAndLower(gchar *input)
 {
-	/* Implement Here! */
-	return str;
+	GString *output;
+	gint i, len, start, end;
+
+	output = g_string_new("");
+	len = g_utf8_strlen(input, -1);
+	start = 0;
+	end = len - 1;
+
+	// Strip: remove leading and trailing punctuations not inner ones.
+
+	// From the beginning
+	if (g_ascii_ispunct(input[0])) {
+		for (i = 1; i < len; ++i) {
+			if (!g_ascii_ispunct(input[i])) {
+				break;
+			}
+		}
+		start = i;
+	}
+
+	// From the end
+	if (g_ascii_ispunct(input[len - 1])) {
+		for (i = len - 1; i >= 0; --i) {
+			if (!g_ascii_ispunct(input[i])) {
+				break;
+			}
+		}
+		end = i;
+	}
+
+	for (i = start; i <= end; ++i) {
+		g_string_append_c(output, input[i]);
+	}
+	g_string_append(output, "\0");
+
+	return output;
 }
 
 /*****************************************************************************/
@@ -48,7 +82,10 @@ GString *getContent(gchar *file_name)
 	file_stream = g_file_read(file, NULL, NULL);
 	data_stream = g_data_input_stream_new(G_INPUT_STREAM(file_stream));
 	while ((line = g_data_input_stream_read_line(data_stream, NULL, NULL, NULL))) {
-		content = g_string_append(content, line);
+		gchar *lowered = g_utf8_strdown(line, g_utf8_strlen(line, -1));
+		content = g_string_append(content, lowered);
+		content = g_string_append_c(content, ' ');	// delimiter for line
+		g_free(lowered);
 	}
 
 	g_clear_object(&data_stream);
@@ -64,22 +101,24 @@ GSequence *getFilteredWordSequence(gchar **words)
     gchar **ptr;
 
     sequence = g_sequence_new(g_free);
+    // when g_sequence_free(), all internal data will be freed by g_free()
 
     for (ptr = words; *ptr; ++ptr) {
-    	gchar *new_word;
-    	//DEBUG_PRINT("word in : [%s]\n", *ptr);
+    	GString *new_word;
+    	DEBUG_PRINT("word in : [%s]\n", *ptr);
 
     	// Filter 0 size
     	if (g_utf8_strlen(*ptr, -1) == 0) {
     		continue;
     	}
     	// Strip punctuations
-    	new_word = stripPunctuation(*ptr);
+    	new_word = stripPunctuationAndLower(*ptr);
 
     	// Add to sequence
-        g_sequence_append(sequence, g_strdup(new_word));
+        g_sequence_append(sequence, g_strdup(new_word->str));
+        DEBUG_PRINT("word out: [%s]\n", new_word->str);
 
-        DEBUG_PRINT("word out: [%s]\n", new_word);
+        g_string_free(new_word, TRUE);
     }
 
     return sequence;
@@ -89,7 +128,7 @@ void process(gchar *file_name)
 {
 	GString *content;
 	gchar **words;
-	GSequence *filtered_words;
+	GSequence *word_sequence;
 
 	DEBUG_PRINT("File Name: %s\n", file_name);
 
@@ -100,11 +139,20 @@ void process(gchar *file_name)
 	// Split content to words
 	words = g_strsplit(content->str, " ", 0);
 
-	// Filter words
-	filtered_words = getFilteredWordSequence(words);
+	// Filter words and store to sequence
+	word_sequence = getFilteredWordSequence(words);
+
+	// Free resources
+	g_sequence_free(word_sequence);
+	g_strfreev(words);
+	g_string_free(content, TRUE);
 }
 
-int main(gint argc, gchar *argv[])
+/*****************************************************************************/
+/* Main */
+/*****************************************************************************/
+
+gint main(gint argc, gchar *argv[])
 {
 	// Without this line, there comes weird error in runtime!
 	// Reference: http://ubuntuforums.org/showthread.php?t=1642100
